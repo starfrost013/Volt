d815c37d1bdaac09fbde02ea37568ed1492f288c
starfrost013 <mario64crashed@gmail.com>
2025-08-21 13:06:25 +0100

    oops

--- emulation/machine/machine.cpp
+++ emulation/machine/machine.cpp
@@ -43,6 +43,6 @@ namespace Volt
     // Get around stupid include crap
     void Machine::LogComponent(const char* name)
     {
-        Logging_LogChannel("Successfully added machine component %s", Volt::LogChannel::Debug);
+        Logging_LogChannel("Successfully added machine component %s", Volt::LogChannel::Debug, name);
     }
 }
