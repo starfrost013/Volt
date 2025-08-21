e9efb71f7b386fbe2566895dccbd75a9d46914bf
starfrost013 <mario64crashed@gmail.com>
2025-08-21 21:34:37 +0100

    5x push/pop

--- emulation/components/cpu/cpu8086_ops_stack.cpp
+++ emulation/components/cpu/cpu8086_ops_stack.cpp
@@ -5,10 +5,19 @@
 // cpu8086_ops_move.cpp: 8086 move ops
 //
 
+#include "cpu8086.hpp"
 #include <emulation/emulation.hpp>
 #include <emulation/components/cpu/cpu8086.hpp>
 
 namespace Volt
 {
+    void CPU8086::Op_PushReg(uint8_t opcode)
+    {
+        stack_push_16(*(register_table16[opcode & 0x07]));
+    }
 
+    void CPU8086::Op_PopReg(uint8_t opcode)
+    {
+        *(register_table16[opcode & 0x07]) = stack_pop_16();
+    }
 }
